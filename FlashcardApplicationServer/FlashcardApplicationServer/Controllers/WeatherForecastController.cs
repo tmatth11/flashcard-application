a3b0066b1d2c37f0cd436218aea50b6ec17a26a1
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-04-04 14:25:48 -0500

    Add authentication/authorization

--- FlashcardApplicationServer/FlashcardApplicationServer/Controllers/WeatherForecastController.cs
+++ FlashcardApplicationServer/FlashcardApplicationServer/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlashcardApplicationServer.Controllers
@@ -12,6 +13,7 @@ namespace FlashcardApplicationServer.Controllers
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
+        [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- FlashcardApplicationServer/FlashcardApplicationServer/Program.cs
+++ FlashcardApplicationServer/FlashcardApplicationServer/Program.cs
@@ -1,9 +1,34 @@
+using FlashcardApplicationServer.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAuthorization();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FlashcardAppDb")));
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Flashcard API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer",
+    });
+    options.AddSecurityRequirement(document => new()
+    {
+        [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+    });
+});
 
 var app = builder.Build();
 
@@ -15,7 +40,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapIdentityApi<IdentityUser>();
 app.MapControllers();
 
 app.Run();
